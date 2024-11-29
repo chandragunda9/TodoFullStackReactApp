84f015e1b7b64b1cfcd3b7e0aeef6b4561d5e7af
chandragunda9 <chandragunda1215@gmail.com>
2024-11-29 23:50:15 +0530

    deployment

--- src/components/todo/api/ApiClient.jsx
+++ src/components/todo/api/ApiClient.jsx
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:8080'
+        // baseURL: 'http://localhost:8080'
+        // baseURL: 'http://localhost:5000'
+        //deployed AWS backend URL
+        baseURL: 'http://todoappbackend-env.eba-7thtpjv3.ap-south-1.elasticbeanstalk.com/'
     }
 )
